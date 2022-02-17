2eb08b04c4e007d3f51a8117c68da62e4cfdfd2f
jihyehann <haneh0714@gmail.com>
2022-02-17 14:47:29 +0900

    Add: Header 아이콘 추가

--- Frontend/src/component/common/Header.js
+++ Frontend/src/component/common/Header.js
@@ -5,6 +5,10 @@ class Header extends PureComponent {
         return (
             <div className='header'>
                 <img src='./logo.svg' height={20}/>
+                <div>
+                    <img src='./icons/search.svg' width={48} height={48}/>
+                    <img src='./icons/user.svg' width={48} height={48}/>
+                </div>
             </div>
         )
     }
